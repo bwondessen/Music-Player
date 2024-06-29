3977e2bdb266c8cdcf6cffef0021c69b0203722a
bwondessen <bwondessen23@gmail.com>
2024-06-29 14:32:54 -0500

    Create arrow function with parameters

--- script.js
+++ script.js
@@ -90,8 +90,13 @@ const printGreeting = () => {
 }
 
 printGreeting();
-const printMessage = (org) => {
-    console.log(`${org} is awesome!`);
-};
 
-printMessage("freeCodeCamp");
+const printMessage = org => {
+    console.log(`${org} is awesome!`);
+}
+
+printMessage('freeCodeCamp');
+const addTwoNumbers = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwoNumbers(3, 4));
