927a6dfa6492824ee32fe01a4be786f0ba9cdeb5
bwondessen <bwondessen23@gmail.com>
2024-07-18 00:58:53 -0500

    Add two more span elements in button for artist and duration

--- script.js
+++ script.js
@@ -91,8 +91,11 @@ const renderSongs = (array) => {
         .map((song) => {
             return `
       <li id="song-${song.id}" class="playlist-song">
-<button class="playlist-song-info"><span class="playlist-song-title">${song.title}</span></button>
->      
+      <button class="playlist-song-info">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
       </li>
       `;
         })
