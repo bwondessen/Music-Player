e623c711c662f25bb513f0257003321b8854a217
bwondessen <bwondessen23@gmail.com>
2024-08-07 18:08:56 -0500

    Check if a.title is less than b.title in sort method

--- script.js
+++ script.js
@@ -110,10 +110,10 @@ const renderSongs = (array) => {
 
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
-
-    }
-
-    );
+        if (a.title < b.title) {
+            return -1;
+        }
+    });
 };
 
 renderSongs(userData?.songs);
