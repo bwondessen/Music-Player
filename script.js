73fc5ffcdf01efd57ae1668ea64a5cb309f83b59
bwondessen <bwondessen23@gmail.com>
2024-10-20 01:21:05 -0500

    Display song title and artist when song plays

--- script.js
+++ script.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
     playButton.classList.add("playing");
 
     highlightCurrentSong();
+    setPlayerDisplay();
     audio.play();
 };
 
@@ -135,6 +136,7 @@ const setPlayerDisplay = () => {
     const songArtist = document.getElementById("player-song-artist");
     const currentTitle = userData?.currentSong?.title;
     const currentArtist = userData?.currentSong?.artist;
+
     playingSong.textContent = currentTitle ? currentTitle : "";
     songArtist.textContent = currentArtist ? currentArtist : "";
 };
