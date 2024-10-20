eeec48c13040e45c3b6550fb3e65c26d9d8d2309
bwondessen <bwondessen23@gmail.com>
2024-10-20 02:33:34 -0500

    Update value for aria-label attribute

--- script.js
+++ script.js
@@ -100,6 +100,7 @@ const playSong = (id) => {
 
     highlightCurrentSong();
     setPlayerDisplay();
+    setPlayButtonAccessibleText();
     audio.play();
 };
 
@@ -130,6 +131,8 @@ const playPreviousSong = () => {
         playSong(previousSong.id);
     }
 };
+
+
 
 const setPlayerDisplay = () => {
     const playingSong = document.getElementById("player-song-title");
@@ -174,6 +177,15 @@ const renderSongs = (array) => {
         .join("");
 
     playlistSongs.innerHTML = songsHTML;
+};
+
+const setPlayButtonAccessibleText = () => {
+    const song = userData?.currentSong || userData?.songs[0];
+
+    playButton.setAttribute(
+        "aria-label",
+        song?.title ? `Play ${song.title}` : "Play"
+    );
 };
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
